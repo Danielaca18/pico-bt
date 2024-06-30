aa839dcaac2279f00bad6ea4c9a5be5cb449a1c6
Daniel Castro <dcastro@ualberta.ca>
2024-06-29 20:27:52 -0600

    added code from bluekitchen example code

--- src/main.c
+++ src/main.c
@@ -1,1 +1,16 @@
-int main() {};
+#include <stdio.h>
+#include "btstack.h"
+#include "pico/cyw43_arch.h"
+#include "pico/btstack_cyw43.h"
+#include "pico/stdlib.h"
+
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+int main() {
+    stdio_init_all();
+
+    if (cyw43_arch_init()) return -1;
+
+    l2cap_init();
+    sm_init();
+};
